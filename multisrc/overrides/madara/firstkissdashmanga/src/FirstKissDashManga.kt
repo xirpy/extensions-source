264e2d637c48ab48443a09e8f5dc7f12f66b905a
Xirpy <39977285+xirpy@users.noreply.github.com>
2024-02-11 13:17:33 -0300

    Update FirstKissDashManga.kt

--- multisrc/overrides/madara/firstkissdashmanga/src/FirstKissDashManga.kt
+++ multisrc/overrides/madara/firstkissdashmanga/src/FirstKissDashManga.kt
@@ -2,7 +2,7 @@ package eu.kanade.tachiyomi.extension.en.firstkissdashmanga
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
-class FirstKissDashManga : Madara("1st Kiss-Manga (unoriginal)", "https://1stkiss-manga.com", "en") {
+class FirstKissDashManga : Madara("1st Kiss-Manga (unoriginal)", "https://1st-kissmanga.online", "en") {
     override val useNewChapterEndpoint = false
 
     override fun searchPage(page: Int): String = if (page == 1) "" else "page/$page/"
